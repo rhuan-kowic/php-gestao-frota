9c6fee89106981bcc3f0cf8114912de05a618b5f
Rhuan Kowic <dev.rhuankowic@gmail.com>
2026-06-20 20:34:57 -0300

    feat: adiciona template navbar.

---
+++ public/navbar.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="./assets/styles/navbar.css">
+<div class="nav-bar">
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="vehiclesList.php">Lista</a>
+    </nav>
+</div>
